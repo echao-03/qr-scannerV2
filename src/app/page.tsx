15bc19f5ab8a28bef028ea82ad0044e690b5d551
Erik Chao <etchao001@gmail.com>
2025-05-28 14:44:03 -0700

    removed reactNode component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState, ReactNode } from "react";
+import React, { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 
 import { Suspense } from "react";
